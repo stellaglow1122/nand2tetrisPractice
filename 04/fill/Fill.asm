e8e0a57b0ef7df3cf4d198e2368d8b13420cccca
陳弘翔 <33833058+stellaglow1122@users.noreply.github.com>
2023-08-30 13:53:17 +0800

    Completed Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// LoopForInput:
+//        if RAM[KBD] != 0:
+//            screenaddress = SCREEN
+//            LoopForBlack:
+//                if screenaddress > SCREEN + 8191:
+//                    goto LoopForInput
+//                RAM[screenaddress] = -1
+//                screenaddress = screenaddress + 1
+//                goto LoopForBlack
+//            screenaddress = SCREEN
+//            LoopForWhite:
+//                if screenaddress > SCREEN + 8191:
+//                    goto LoopForInput
+//                RAM[screenaddress] = 0
+//                screenaddress = screenaddress + 1
+//                goto LoopForWhite
+(LoopForInput)
+    @KBD
+    D = M // D = RAM[KBD]
+    @InitLoopForWhite
+    D;JEQ // if D == 0, goto InitLoopForWhite
+    @SCREEN
+    D = A // D = SCREEN
+    @screenaddress
+    M = D // screenaddress = D
+    (LoopForBlack)
+        @screenaddress
+        D = M // D = screenaddress
+        @8191
+        D = D - A // screenaddress = screenaddress - 8191
+        @SCREEN
+        D = D - A // screenaddress = screenaddress - SCREEN
+        @LoopForInput
+        D;JGT // if screenaddress - SCREEN - 8191 > 0; goto LoopForInput
+        @screenaddress
+        A = M
+        M = -1
+        @screenaddress
+        M = M + 1
+        @LoopForBlack
+        0;JMP
+    (InitLoopForWhite)
+        @SCREEN
+        D = A
+        @screenaddress
+        M = D
+    (LoopForWhite)
+        @screenaddress
+        D = M
+        @8191
+        D = D - A
+        @SCREEN
+        D = D - A
+        @LoopForInput
+        D;JGT
+        @screenaddress
+        A = M
+        M = 0
+        @screenaddress
+        M = M + 1
+        @LoopForWhite
+        0;JMP
