96b6c0ab229e683539adeb85226030fca63dc271
陳弘翔 <33833058+stellaglow1122@users.noreply.github.com>
2023-08-30 10:29:40 +0800

    Completed Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,53 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// i = 1
+// sum = 0
+// n = R1
+// Loop:
+//      if i > n:
+//          goto Stop
+//      sum = sum + R0
+//      goto Loop
+// Stop:
+//      R2 = sum
+// End:
+//      goto End
+@i
+M = 1 // i = 1
+
+@sum
+M = 0 // sum = 0
+
+@R1
+D = M // D = R1
+@n
+M = D // n = R1
+
+(Loop)
+    @i
+    D = M // D = i
+    @n
+    D = D - M   // i = i - n
+    @Stop
+    D;JGT   // if i > n, goto Stop
+
+    @sum
+    D = M // D = sum
+    @R0
+    D = D + M  // D = D + R0
+    @sum
+    M = D // sum = D
+    @i
+    M = M + 1 // i = i + 1
+    @Loop
+    0;JMP
+(Stop)
+    @sum
+    D = M // D = sum
+    @R2
+    M = D // R2 = sum
+(End)
+    @End
+    0;JMP
